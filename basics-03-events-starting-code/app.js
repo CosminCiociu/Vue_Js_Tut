90bb768ff4ed7ca427306d5f15c303d0bd2ea780
CosminCiociu <tutu.cosmy@gmail.com>
2023-01-24 12:40:36 +0200

    watchers/computed

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -4,9 +4,19 @@ const app = Vue.createApp({
       counter: 1,
       name: "",
       confirmedName: "",
+      //   fullName: "",
+      lastName: "",
     };
   },
   methods: {
+    outpuFullName() {
+      console.log("Running again");
+      if (this.name === "") {
+        return "";
+      } else {
+        return `${this.name} Cosmin`;
+      }
+    },
     addValue(num) {
       return (this.counter += num);
     },
@@ -26,6 +36,39 @@ const app = Vue.createApp({
     resetInput() {
       this.name = "";
     },
+  },
+  computed: {
+    fullName() {
+      console.log("Running again");
+      if (this.name === "" || this.lastName === "") {
+        return "";
+      } else {
+        return `${this.name} ${this.lastName}`;
+      }
+    },
+  },
+  watch: {
+    counter(value) {
+      if (value > 15) {
+        setTimeout(() => {
+          this.counter = 0;
+        }, 2000);
+      }
+    },
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullName = "";
+    //   } else {
+    //     this.fullName = `${value} ${this.lastName}`;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === "") {
+    //     this.fullName = "";
+    //   } else {
+    //     this.fullName = `${value} ${this.name}`;
+    //   }
+    // },
   },
 });
 
