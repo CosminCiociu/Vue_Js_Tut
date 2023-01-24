ce0c85ef76a4dcbe252fd0ad89662e6cbb5159fc
CosminCiociu <tutu.cosmy@gmail.com>
2023-01-24 12:11:36 +0200

    Events, data binding

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,20 +2,31 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 1,
-      name: ''
+      name: "",
+      confirmedName: "",
     };
   },
-  methods:{
+  methods: {
     addValue(num) {
-      return this.counter += num;
+      return (this.counter += num);
     },
     reduceValue(num) {
-      return this.counter -= num;
+      return (this.counter -= num);
     },
     setName(event, lastName) {
-      return this.name = `${event.target.value} ${lastName}`;
-    }
+      return (this.name = `${event.target.value} ${lastName}`);
+    },
+    submitFrom(event) {
+      // event.preventDefault();
+      alert("Form Submited");
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    resetInput() {
+      this.name = "";
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
