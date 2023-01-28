8a60d7293c72112f5852f9050d8832a192dad51b
CosminCiociu <tutu.cosmy@gmail.com>
2023-01-28 17:24:02 +0200

    First vue project with vue cli

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <section>
+        <header>
+            <h1> My Friends</h1>
+        </header>
+        <ul>
+            <friend-contact></friend-contact>
+            <friend-contact></friend-contact>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '0720283037',
+                    email: 'mauel@gmail.com'
+                },
+                {
+                    id: 'julie',
+                    name: 'Julie Morenz',
+                    phone: '073 428 3067',
+                    email: 'julie@gmail.com'
+                },
+            ]
+        }
+    }
+}
+</script>
