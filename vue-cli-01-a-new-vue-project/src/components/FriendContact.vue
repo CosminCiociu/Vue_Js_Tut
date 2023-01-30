7c0669dd6c6c89f34f486ef738f93f0f854c5927
CosminCiociu <tutu.cosmy@gmail.com>
2023-01-30 15:22:10 +0200

    Components props emits

--- vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,30 +1,72 @@
 <template>
     <li>
-        <h2>{{ friend.name }}</h2>
+        <h2>{{ name }} {{ isFavorite? "(Favorite)": "" }}</h2>
+        <button @click="toggleFavorite">Toggle Favorite</button>
         <button @click="toggleDetails"> Show Details</button>
         <ul v-if="detailsAreVisible">
-            <li><strong>Phone:</strong>{{ friend.phone }}</li>
-            <li><strong>Email:</strong>{{ friend.email }}</li>
+            <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+            <li><strong>Email:</strong> {{ emailAddress }}</li>
         </ul>
+        <button @click="$emit('delete-friend', id)">Delete Friend</button>
     </li>
 </template>
 
 <script>
 export default {
+    // props: [
+    //     'name',
+    //     'phoneNumber',
+    //     'emailAddress',
+    //     'isFavorite'
+    // ],
+    props: {
+        'id' : {
+            type: String,
+            required: true
+        },
+        'name': {
+            type: String,
+            required: true
+        },
+        'phoneNumber': {
+            type: String,
+            required: true
+        },
+        'emailAddress': {
+            type: String,
+            required: true
+        },
+        'isFavorite': {
+            type: Boolean,
+            required: false,
+            default: false,
+            // validator: function(value) {
+            //     return value === '1' || value === '0';
+            // }
+        }
+    },
+    emits: ["toggle-favorite", "delete-friend"],  
+    // emits: {
+    //     'toggle-favorite' : function(id) {
+    //         if(id) {
+    //             return true;
+    //         }else {
+    //             console.warn('Id is missing')
+    //             return false
+    //         }
+    //     }
+    // },  
     data() {
         return {
             detailsAreVisible: false,
-            friend: {
-                id: 'manuel',
-                name: 'Manuel Lorenz',
-                phone: '0720283037',
-                email: 'mauel@gmail.com'
-            }
         }
     },
     methods: {
         toggleDetails() {
             this.detailsAreVisible = !this.detailsAreVisible;
+        },
+        toggleFavorite() {
+            this.$emit('toggle-favorite', this.id);
         }
     }
 }
@@ -93,5 +135,8 @@ header {
     background-color: #ec3169;
     border-color: #ec3169;
     box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+button {
+    margin-right: 10px;
 }
 </style>
