e551a671c3d78a3648713cd0b5f5db4eb2c91b62
CosminCiociu <tutu.cosmy@gmail.com>
2023-02-14 20:57:51 +0200

    Ended transition course about vue

--- animations-01-starting-setup/src/App.vue
+++ animations-01-starting-setup/src/App.vue
@@ -1,15 +1,40 @@
 <template>
+  <router-view v-slot="slotProps">
+    <transition name="route" mode="out-in">
+      <component :is="slotProps.Component"> </component>
+    </transition>
+  </router-view>
+  <div class="container">
+    <users-list></users-list>
+  </div>
   <div class="container">
     <div class="block" :class="{ animate: animateBlock }"></div>
     <button @click="animatedBlock">Animate</button>
   </div>
   <div class="container">
-    <transition>
+    <transition
+      :css="false"
+      name="para"
+      @before-enter="beforeEnter"
+      @before-leave="beforeLeave"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @leave="leave"
+      @after-leave="afterLeave"
+      @enter-cancelled="enterCancel"
+      @leave-cancelled="leaveCancel"
+    >
       <p v-if="paraIsVisible">This is sometimes visible</p>
     </transition>
     <button @click="togglePara">Toggle Paragraph</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
@@ -19,14 +44,20 @@
 </template>
 
 <script>
+import UsersList from './components/UsersList.vue';
+
 export default {
   data() {
     return {
       dialogIsVisible: false,
       animateBlock: false,
       paraIsVisible: false,
+      usersAreVisible: false,
+      enterInterval: null,
+      leaveInterval: null,
     };
   },
+  components: { UsersList },
   methods: {
     showDialog() {
       this.dialogIsVisible = true;
@@ -39,6 +70,63 @@ export default {
     },
     togglePara() {
       this.paraIsVisible = !this.paraIsVisible;
+    },
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
+    },
+    beforeEnter(el) {
+      console.log('Before enter');
+      console.log(el);
+      el.style.opacity = 0;
+    },
+    beforeLeave(el) {
+      console.log('Before leave');
+      console.log(el);
+    },
+    enter(el, done) {
+      console.log('Enter');
+      console.log(el);
+      let round = 1;
+      this.enterInterval = setInterval(() => {
+        el.style.opacity = round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.enterInterval);
+          done();
+        }
+      }, 20);
+    },
+    afterEnter(el) {
+      console.log('After Enter');
+      console.log(el);
+    },
+    leave(el, done) {
+      console.log('Leave');
+      console.log(el);
+      let round = 1;
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      }, 20);
+    },
+    afterLeave(el) {
+      console.log('After Leave');
+      console.log(el);
+    },
+    enterCancel(el) {
+      console.log(el);
+      clearImmediate(this.enterInterval);
+    },
+    leaveCancel(el) {
+      console.log(el);
+      clearImmediate(this.leaveInterval);
     },
   },
 };
@@ -88,9 +176,42 @@ button:active {
 }
 .animate {
   /* transform: translateX(-150px); */
-  animation: slide-fade 0.3s ease-out forwards;
+  animation: slide-scale 0.3s ease-out forwards;
 }
-@keyframes slide-fade {
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
+}
+
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.route-enter-from,
+.route-leave-to {
+  opacity: 0;
+}
+.route-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+.route-enter-to,
+.route-leave-from {
+  opacity: 1;
+}
+
+.route-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1);
   }
--- animations-01-starting-setup/src/main.js
+++ animations-01-starting-setup/src/main.js
@@ -1,10 +1,30 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
 import BaseModal from './components/BaseModal.vue';
+import AllUsers from './pages/AllUsers.vue';
+import CourseGoals from './pages/CourseGoals.vue';
 
 const app = createApp(App);
 
-app.component('base-modal', BaseModal);
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: AllUsers,
+    },
+    {
+      path: '/course-goals',
+      component: CourseGoals,
+    },
+  ],
+});
+app.use(router);
 
-app.mount('#app');
+router.isReady().then(() => {
+  app.mount('#app');
+});
+
+app.component('base-modal', BaseModal);
